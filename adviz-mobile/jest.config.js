5235094cbcf5b3df11bbc086449ebbf6f4e33fac
Kirill Ibragimov <hexchap@gmail.com>
2026-05-18 09:39:09 +0300

    fix(mobile): exclude e2e dir from unit test runner

--- adviz-mobile/jest.config.js
+++ adviz-mobile/jest.config.js
@@ -7,4 +7,9 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'test-utils\\.tsx?$',
+    '/e2e/',
+  ],
 };
