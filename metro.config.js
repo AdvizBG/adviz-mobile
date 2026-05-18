35f1b6bc47df33e9b53e7ba490008ce1132778cc
Kirill Ibragimov <hexchap@gmail.com>
2026-05-18 10:50:33 +0300

    chore(mobile): upgrade to Expo SDK 55, remove buffer polyfill (fixed in react-native-svg 15.15.3)

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  buffer: require.resolve('buffer'),
-};
-
 module.exports = withNativeWind(config, { input: './global.css' });
